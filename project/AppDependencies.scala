a9cf2b35f8251009fdbbc86eef16f6e556d6e061
rajanimohan22 <git@github.com>
2019-02-13 13:26:40 +0000

    Test cases added

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -21,7 +21,7 @@ object AppDependencies {
     "org.pegdown"               %  "pegdown"                  % "1.6.0"                 % "test, it",
     "uk.gov.hmrc"               %% "service-integration-test" % "0.2.0"                 % "test, it",
     "org.scalatestplus.play"    %% "scalatestplus-play"       % "2.0.0"                 % "test, it",
-    "org.mockito"               % "mockito-core"              % "2.13.0"                % "test, it",
+    "org.mockito"               % "mockito-core"              % "2.13.0"                % "test, it"
   )
 
 }
