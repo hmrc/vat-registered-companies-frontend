277f5fab71218e92a86d4d072a1929f09a78df16
rajanimohan22 <mohan.rao.dolla@digital.hmrc.gov.uk>
2019-02-19 17:24:07 +0000

    Test Coverage upto 80%

--- test/uk/gov/hmrc/vatregisteredcompaniesfrontend/controllers/VatRegCoLookupControllerSpec.scala
+++ test/uk/gov/hmrc/vatregisteredcompaniesfrontend/controllers/VatRegCoLookupControllerSpec.scala
@@ -20,22 +20,20 @@ import java.time.{LocalDateTime, ZoneId}
 
 import org.mockito.ArgumentMatchers.{any, eq => matching}
 import org.mockito.Mockito.when
-import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach, Matchers, WordSpec}
+import org.scalatest.{ Matchers, WordSpec}
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
 import play.api.http.Status
 import play.api.i18n.{DefaultLangs, DefaultMessagesApi}
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
-import play.api.{Configuration, DefaultApplication, Environment}
+import play.api.{Configuration, Environment}
 import uk.gov.hmrc.vatregisteredcompaniesfrontend.config.AppConfig
-import uk.gov.hmrc.vatregisteredcompaniesfrontend.connectors.VatRegisteredCompaniesConnector
 import uk.gov.hmrc.vatregisteredcompaniesfrontend.models.{ConsultationNumber, Lookup, _}
 import utils.TestWiring
 
-import scala.collection.immutable.Stream.Empty
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.Future
 
-class VatRegCoLookupControllerSpec extends WordSpec with Matchers with GuiceOneAppPerSuite with TestWiring with BeforeAndAfterEach {
+class VatRegCoLookupControllerSpec extends WordSpec with Matchers with GuiceOneAppPerSuite with TestWiring {
 
 
   val fakeRequest = FakeRequest("GET", "/")
