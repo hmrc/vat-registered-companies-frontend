251b8b8008389ea8a66df216a33240f4a08ca6aa
Matt Roberts <mattjr72@gmail.com>
2019-03-12 17:00:54 +0000

    Uvl 124 google analytics (#15)
    * adding gtag and one event
    
    * fixing event parameters
    
    * WIP
    
    * google analytics
    
    * delete unused helper
    
    * fixed wrap on consultation number
    
    * added GA event for search

--- public/javascript/vatcheck-frontend-app.js
+++ public/javascript/vatcheck-frontend-app.js
@@ -11,3 +11,21 @@ if (document.addEventListener) {
         showHide()
     })
 }
+
+window.onload = function() {
+
+    if(document.getElementById('get-help-action')) {
+        document.getElementById('get-help-action').addEventListener('click', function() {
+            ga('send', 'event', 'link-click', document.title, 'Get help with this page');
+        });
+    }
+
+    if(document.getElementById('exit-survey')) {
+        document.getElementById('exit-survey').addEventListener('click', function() {
+            ga('send', 'event', 'link-click', document.title, 'What did you think of this service');
+        });
+    }
+
+
+}
+
