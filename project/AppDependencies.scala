72d9fa2d953a1f627e5dec3cfae1c2323164522f
Priyanka Dhiman <88194246+priyankadhiman3006@users.noreply.github.com>
2022-08-01 11:36:22 +0100

    DLS-5500 play upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,20 +5,22 @@ import sbt._
 
 object AppDependencies {
 
+  val bootstrapVersion = "6.4.0"
+
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28"    % "5.24.0",
-    "uk.gov.hmrc"             %% "play-frontend-hmrc"            % "3.15.0-play-28",
+    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28"    % bootstrapVersion,
+    "uk.gov.hmrc"             %% "play-frontend-hmrc"            % "3.22.0-play-28",
     "uk.gov.hmrc"             %% "play-conditional-form-mapping" % "1.11.0-play-28",
     "uk.gov.hmrc"             %% "play-language"                 % "5.2.0-play-28",
     "uk.gov.hmrc"             %% "simple-reactivemongo"          % "8.0.0-play-28",
-    "uk.gov.hmrc"             %% "mongo-caching"                 % "7.1.0-play-28",
-    "org.typelevel"           %% "cats-core"                     % "2.4.2",
+    "uk.gov.hmrc"             %% "mongo-caching"                 % "7.2.0-play-28",
+    "org.typelevel"           %% "cats-core"                     % "2.7.0",
     compilerPlugin("com.github.ghik" % "silencer-plugin" % "1.7.5" cross CrossVersion.full),
     "com.github.ghik" % "silencer-lib" % "1.7.5" % Provided cross CrossVersion.full
   )
 
   val test = Seq(
-    "uk.gov.hmrc"               %% "bootstrap-test-play-28"   % "5.24.0"           % "test",
+    "uk.gov.hmrc"               %% "bootstrap-test-play-28"   % bootstrapVersion  % "test",
     "org.scalatest"             %% "scalatest"                % "3.2.9"           % "test",
     "org.jsoup"                 %  "jsoup"                    % "1.14.1"          % "test",
     "com.typesafe.play"         %% "play-test"                % current           % "test",
