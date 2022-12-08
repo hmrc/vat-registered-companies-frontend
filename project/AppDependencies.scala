175e5b315b2b2c5d79f312916dfd27e45b4ab517
Ginny Thomas <90277886+ginnyamazed@users.noreply.github.com>
2022-12-08 16:31:03 +0000

    DLS-6478 update sbt libraries where possible

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,11 +10,11 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-frontend-play-28"    % bootstrapVersion,
-    "uk.gov.hmrc"             %% "play-frontend-hmrc"            % "3.23.0-play-28",
+    "uk.gov.hmrc"             %% "play-frontend-hmrc"            % "3.34.0-play-28",
     "uk.gov.hmrc"             %% "play-conditional-form-mapping" % "1.12.0-play-28",
     "uk.gov.hmrc"             %% "play-language"                 % "5.4.0-play-28",
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"            % hmrcMongoVersion,
-    "org.typelevel"           %% "cats-core"                     % "2.8.0",
+    "org.typelevel"           %% "cats-core"                     % "2.9.0",
     compilerPlugin("com.github.ghik" % "silencer-plugin" % "1.7.5" cross CrossVersion.full),
     "com.github.ghik" % "silencer-lib" % "1.7.5" % Provided cross CrossVersion.full
   )
