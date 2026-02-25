6cfe8461f73f6901b2c9263e36815cf23fa5e86a
ZaraFarzan <177644563+ZaraFarzan@users.noreply.github.com>
2026-02-25 15:16:23 +0000

    DLS-12759:[VAT] Upgrade to bootstrap-play (10.6.0)
    DLS-12759:[VAT] Upgrade to bootstrap-play (10.6.0)
    
    DLS-12759:[VAT] Upgrade to bootstrap-play (10.6.0)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,13 +4,13 @@ import sbt._
 object AppDependencies {
 
   private val playVersion = "play-30"
-  val bootstrapVersion = "10.4.0"
-  val hmrcMongoVersion = "2.10.0"
+  val bootstrapVersion = "10.6.0"
+  val hmrcMongoVersion = "2.12.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"             %% s"bootstrap-frontend-$playVersion"            % bootstrapVersion,
-    "uk.gov.hmrc"             %% s"play-frontend-hmrc-$playVersion"            % "12.20.0",
-    "uk.gov.hmrc"             %% s"play-conditional-form-mapping-$playVersion" % "3.3.0",
+    "uk.gov.hmrc"             %% s"play-frontend-hmrc-$playVersion"            % "12.31.0",
+    "uk.gov.hmrc"             %% s"play-conditional-form-mapping-$playVersion" % "3.4.0",
     "uk.gov.hmrc.mongo"       %% s"hmrc-mongo-$playVersion"                    % hmrcMongoVersion,
     "org.typelevel"           %% "cats-core"                                   % "2.13.0",
     "com.digitaltangible"     %% "play-guard"                                  % "3.0.0"
@@ -19,8 +19,8 @@ object AppDependencies {
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"               %% s"bootstrap-test-$playVersion" % bootstrapVersion,
     "org.playframework"         %% "play-test"                    % current,
-    "org.mockito"               %  "mockito-core"                 % "5.19.0",
-    "org.jsoup"                 %  "jsoup"                        % "1.21.2",
+    "org.mockito"               %  "mockito-core"                 % "5.21.0",
+    "org.jsoup"                 %  "jsoup"                        % "1.22.1",
     "com.vladsch.flexmark"      % "flexmark-all"                  % "0.64.8"
   ).map(_ % "test, it")
 
